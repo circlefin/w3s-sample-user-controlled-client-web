5af82b31fd9e99ffbbc57544a7bbdec28c6531bc
undefined <circle-github-actions@circle.com>
2024-05-24 15:43:54 +0000

    🔄 synced local 'tailwind.config.ts' with remote 'tailwind.config.ts'

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        secondary: "#9C27B0",
+      },
     },
   },
   plugins: [],
